35708534e1696235c70f22a35a925ca68daef19b
shivam907 <shivamkaushal907@gmail.com>
2022-12-04 19:32:32 +0530

    An Update

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,6 @@ window.addEventListener("load", function () {
       method: "POST",
       body: data,
     }).then(() => {
-      //   alert("Success!");
       document.querySelector(".alert").classList.remove("op");
       document.querySelector(".alert").classList.add("zi");
       setTimeout(function () {
@@ -25,9 +24,6 @@ window.addEventListener("load", function () {
 document.querySelector(".dropdown").addEventListener("mouseover", callback);
 
 function callback() {
-  // Get the dropdown menu height when scroll. The default height set by css to 0.
-
   let dropdownHeight = this.children[1].scrollHeight;
-  // Add the height to dropdown menu (change it from 0 to scroll height value )
   this.children[1].style = `height: ${dropdownHeight}px`;
 }
